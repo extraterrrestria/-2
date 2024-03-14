ff004487819c944293c831192eb00654d44bce45
extraterrrestria <114211155+extraterrrestria@users.noreply.github.com>
2024-03-14 19:56:38 +0300

    two tasks

---
+++ task_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    unordered_set<int> unique_nums;
+    for (int i = 0; i < N; i++) {
+        int num;
+        cin >> num;
+        unique_nums.insert(num);
+    }
+
+    cout << unique_nums.size() << endl;
+
+    return 0;
+}
---
+++ task_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    unordered_set<string> stringSet;
+
+    string operation, word;
+    cin >> operation;
+
+    while (operation != "#") {
+        cin >> word;
+        if (operation == "+") {
+            stringSet.insert(word);
+        }
+        else if (operation == "-") {
+            stringSet.erase(word);
+        }
+        else if (operation == "?") {
+            if (stringSet.count(word) > 0) {
+                cout << "YES" << endl;
+            }
+            else {
+                cout << "NO" << endl;
+            }
+        }
+        cin >> operation;
+    }
+
+    return 0;
+}
